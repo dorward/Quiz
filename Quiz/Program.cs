3c5e42e1fc63be37c8b15332d72c8e43137e064c
Kindlewing <hkfinn@outlook.com>
2018-10-04 09:55:59 -0400

    Created the project

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.IO;
+using System;
+
 
 namespace Quiz
 {
@@ -10,6 +9,7 @@ namespace Quiz
 	{
 		static void Main(string[] args)
 		{
+
 		}
 	}
 }
